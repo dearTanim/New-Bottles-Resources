34e1146088af848de815d7f16293afa62595d3ff
Md Asraful Islam <deartanim@gmail.com>
2024-10-23 19:56:36 +0600

    first commit

---
+++ src/component/Bottles/Bottles.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Bottles = () => {
+    const [bottles, setBottles] = useState([])
+    return (
+        <div>
+           <h2>All Bottles Load Here</h2> 
+        </div>
+    );
+};
+
+export default Bottles;
